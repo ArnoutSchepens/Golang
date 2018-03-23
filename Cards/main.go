8e7d9801459ebaca4394e9273bad8eeec46832fb
Seviran <schepens.arnout@gmail.com>
2018-03-23 21:54:41 +0100

    Functions in Go

--- Cards/main.go
+++ Cards/main.go
@@ -1,13 +1,15 @@
 package main
 
-import "fmt"
-
-var deckSize int
+import (
+	"fmt"
+)
 
 func main() {
 	// var card string = "Ace of Spades"
-	card := "Ace of Spades"
+	card := newCard()
 	fmt.Println(card)
-	card = "Five of Diamonds"
-	fmt.Println(card)
+}
+
+func newCard() string {
+	return "Five of Diamonds"
 }
