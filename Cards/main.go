79ebb853598d77856dece9f2619426603687a924
Seviran <schepens.arnout@gmail.com>
2018-03-23 21:45:47 +0100

    Assigning variables

---
+++ Cards/main.go
@@ -0,0 +1,13 @@
+package main
+
+import "fmt"
+
+var deckSize int
+
+func main() {
+	// var card string = "Ace of Spades"
+	card := "Ace of Spades"
+	fmt.Println(card)
+	card = "Five of Diamonds"
+	fmt.Println(card)
+}
