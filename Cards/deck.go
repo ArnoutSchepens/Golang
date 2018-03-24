9700ee83eb22d01eb04ee45745f92d0db651bcba
Seviran <schepens.arnout@gmail.com>
2018-03-24 19:07:05 +0100

    newDeck

--- Cards/deck.go
+++ Cards/deck.go
@@ -8,8 +8,24 @@ import (
 // which is a slice of strings
 type deck []string
 
+func newDeck() deck {
+	cards := deck{}
+
+	cardSuits := []string{"Clubs", "Diamonds", "Hearts", "Spades"}
+	cardValues := []string{"Ace", "Two", "Three", "Four", "Five", "Six", "Seven", "Eight", "Nine", "Ten", "Jack", "Queen", "King"}
+
+	for _, suit := range cardSuits {
+		for _, value := range cardValues {
+			card := value + " of " + suit
+			cards = append(cards, card)
+		}
+	}
+
+	return cards
+}
+
 func (d deck) print() {
-	for i, card := range d {
-		fmt.Println(i, card)
+	for _, card := range d {
+		fmt.Println(card)
 	}
 }
